4daed1909a1a2fbc1a938d6c32d9d50a62dc8707
sky_lock <sho59sky@outlook.jp>
2016-11-27 21:05:40 +0900

    PlayerQuitEventの処理を追加しました

--- src/main/java/mc/sky_lock/parkour/listener/PlayerListener.java
+++ src/main/java/mc/sky_lock/parkour/listener/PlayerListener.java
@@ -2,9 +2,12 @@ package mc.sky_lock.parkour.listener;
 
 import mc.sky_lock.parkour.ParkourHandler;
 import mc.sky_lock.parkour.ParkourManager;
+import mc.sky_lock.parkour.ParkourPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -25,6 +28,17 @@ public class PlayerListener implements Listener {
     @EventHandler
     public void playerMove(PlayerMoveEvent event) {
         manager.measure(event);
+    }
+
+    @EventHandler
+    public void playerLogout(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        for (ParkourPlayer parkourPlayer : handler.getParkourPlayers()) {
+            if (parkourPlayer.getPlayer().equals(player)) {
+                handler.getParkourPlayers().remove(parkourPlayer);
+                return;
+            }
+        }
     }
 
 }
